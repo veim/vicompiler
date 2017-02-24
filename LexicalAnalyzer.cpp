28f788a0c76f822a924213a2e348306397609ba6
veim <veiveim@163.com>
2017-02-25 00:10:36 +0800

    add re

--- LexicalAnalyzer.cpp
+++ LexicalAnalyzer.cpp
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 
 /* maintain a NFA state, all live NFA states make up a list */
 struct re_state{
@@ -7,9 +10,12 @@ struct re_state{
 	struct re_state* next;
 };
 
-/* insert node after current one, and return original next ptr */
+/* insert node after current one */
+/* return original next ptr */
 struct re_state* insert_state(struct re_state* cur, int idx, char* re){
-	struct re_state* ptr = (struct re_state*) malloc(sizeof(struct re_state));
+	struct re_state* ptr = cur;
+	
+	ptr = (struct re_state*) malloc(sizeof(struct re_state));
 	ptr->next_idx = idx;
 	ptr->re[0] = re[0];
 	ptr->re[1] = re[1];
@@ -21,7 +27,8 @@ struct re_state* insert_state(struct re_state* cur, int idx, char* re){
 	return ptr->next;
 }
 
-/* delete current node, and return original next ptr */
+/* delete current node */
+/* return original next ptr */
 struct re_state* delete_state(struct re_state* cur){
 	struct re_state* ptr = cur->next;
 	if(cur->prev != NULL)
@@ -30,86 +37,127 @@ struct re_state* delete_state(struct re_state* cur){
 	return ptr;
 }
 
-/*
-char* next_re(char* p, int idx){
+/* remove repeated states */
+void check_state(struct re_state* head, struct re_state* rear){
+	struct re_state* ptr_i = head;
+	struct re_state* ptr_j;
 
-}
-*/
+	while(ptr_i != rear){
+		ptr_j = ptr_i->next;
 
-struct re_state* forward(char* p , struct re_state** cur){
-	if(p[cur->next_idx+1] == '*'){
-		cur = insert_state(cur, cur->next_idx + 2, p + cur->next_idx);
-		return forward(p, cur);
-	}
-	else{
-		return insert_state(cur, cur->next_idx + 1, p + cur->next_idx);
-	}
+//		printf("re: %c%c, idx:%d\n", ptr_i->re[0], ptr_i->re[1], ptr_i->next_idx);
 
-}
-
-int isFollow(char* p, struct re_state** cur, char c){
-	if(cur->re != NULL && (cur->re[0] == '.' || cur->re[0] == c)){
-		if(p[cur->next_idx] != '\0'){
-			if(p[cur->next_idx+1] == '*')
-				insert_state(cur, cur->next_idx + 2, p + cur->next_idx);
-			else{
-				insert_state(cur, cur->next_idx + 1, p + cur->next_idx);
-			}
-			cur = forward_state(p, cur);
+		while(ptr_j != rear){
+			if(ptr_i->next_idx == ptr_j->next_idx)
+				ptr_j = delete_state(ptr_j);
+			else 
+				ptr_j = ptr_j->next;
 		}
-		if(cur->re[1] == '*')
-			return 2;
-		else
-			return 1;
+		ptr_i = ptr_i->next;
 	}
-	return 0;
 }
+
+
+/* transfer state according to the re and the c */
+/* certain new states may be added if there's continuous '*'s */
+/* return 0 if c doesn't follow re */
+int forward_state(char* p , struct re_state* cur, char c, int* max_idx){
+	int idx = cur->next_idx;
+	struct re_state* ptr = cur;
+
+	/* check if the c follows the re in cur state */
+	if(	!(cur->re[0] == c 
+		|| cur->re[0] == '.' && c != '\0' 
+		|| cur->re[1] == '*' && c == '\0')){
+		return 0;
+	}
+
+	/* udpate max followed idx in p */
+	if(cur->next_idx > *max_idx)
+		*max_idx = cur->next_idx;
+
+	/* transfer states */
+	while(p[idx] != '\0' && p[idx + 1] == '*'){
+		insert_state(ptr, idx + 2, p + idx);
+		ptr = ptr->next;
+		idx += 2;
+	}
+	if(p[idx] != '\0'){
+		insert_state(ptr, idx + 1, p + idx);
+	}
+
+	return 1;
+}
+
 
 bool isMatch(char* s, char* p) {
+	int result, live = 0, flag = 0, i = 0, max_idx = -1;	
+
+	if(s[0] == '\0' && p[0] == '\0')
+		return true;
+
+
 	struct re_state* state_head = (struct re_state*) malloc(sizeof(struct re_state));
 	struct re_state* state_rear = (struct re_state*) malloc(sizeof(struct re_state));
 	state_head->prev = NULL;
 	state_head->next = state_rear;
-	state_rear->prev = NULL;
-	state_rear->next = state_rear;
+	state_rear->prev = state_head;
+	state_rear->next = NULL;
 
-	if(p[1] == '*'){
-		state_head->re[0] = p[0];
-		state_head->re[1] = p[1];
-		state_head->next_idx = 2;
-	}
-	else{
-		state_head->re[0] = p[0];
-		state_head->re[1] = p[1];
-		state_head->next_idx = 1;
-	}
-
-    int flag = 0, result, i = 0;	
+	/* init states list, same as forward_state based on re '.' and the char '\0' */
+	state_head->re[0] = '.';
+	state_head->re[1] = '.';
+	state_head->next_idx = 0;
+	forward_state(p, state_head, '~', &max_idx);
 
     /* iterate string s and maintain live states */
-    while(s[i] != '\0' && state_head != NULL && state_head != state_rear){
-    	struct re_state* state_cur = state_head;
-    	while(state_cur != state_rear){
-    		result = isFollow(p, state_cur, s[i]);
-    		if(result == 2){
-    			flag = 1;
-    			state_cur = state_cur->next;
-    		}
-    		if(result == 1){
-    			flag = 1;
-    			state_cur = delete_state(state_cur);
-    		}
-    		if(result == 0){
-    			state_cur = delete_state(state_cur);
-    		}
+    while(flag != 2 && state_head->next != state_rear){
+    	
+    	struct re_state* ptr = state_head->next;
+    	struct re_state* next = ptr;
+
+    	live = 0;
+    	while(ptr != state_rear){
+    		next = ptr->next;
+    		result = forward_state(p, ptr, s[i], &max_idx);
+
+ 			/* delete cur state if it should not be recycled or not followed*/
+  			if(result == 0 || ptr->re[1] != '*')
+  				delete_state(ptr);
+  
+  			if(result == 1 && p[ptr->next_idx] == '\0'){
+  				live = 1;
+  			}
+  			ptr = next;
   		}
-  		i++;
+
+  		check_state(state_head, state_rear);
+
+  		/* one more forward to deal with specify case: abc, abcd* */
+  		if(s[i] == '\0')
+  			flag++;
+
+  		if(flag == 0)
+  			i++;
 	}
-    if(s[i] == '\0' && flag == 1)
+
+    if(s[i] == '\0' && p[max_idx] == '\0' && live == 1)
     	return true;
     else
     	return false;        	
 }
 
 
+int main(){
+
+	char s[8], p[8];
+	printf("input s: ");
+	scanf("%s", s);
+//	s[0] = '\0';
+	printf("\ninput p: ");
+	scanf("%s", p);
+//	p[0] = '\0';
+	printf("result is: %d\n", (int)isMatch(s, p));
+
+}
 
